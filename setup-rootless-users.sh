04d0e24516c3b97debd19a24de99c20d3900842a
Anthony Schexnaildre <aps@enclarify.com>
2024-01-21 14:22:01 -0800

    Fix sudoers file. Typo like from bad copy/paste

--- setup-rootless-users.sh
+++ setup-rootless-users.sh
@@ -20,7 +20,7 @@ echo 'dockremap:165536:65536' >> /etc/subuid
 echo 'dockremap:165536:65536' >> /etc/subgid
 
 echo "Setting up passwordless sudo"
-echo "%sudo   ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers
+echo "%sudo   ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers
 echo "Defaults !env_reset" >> /etc/sudoers
 echo "Defaults !always_set_home" >> /etc/sudoers
 echo "Defaults env_keep += \"HOME\"" >> /etc/sudoers
